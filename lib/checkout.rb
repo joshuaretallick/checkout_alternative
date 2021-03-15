4c52cc63f533539fde64a50126b320f8130e7953
Joshua Retallick <josh@old-empire.co.uk>
2021-03-15 14:59:37 +0000

    T1

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,3 +1,24 @@
 class Checkout
 
+  def checkout(items)
+    @cost = 0
+    calc(items.to_s.chars)
+    @cost
+  end
+
+  private
+
+  def calc(items)
+    items.each do |item|
+      @cost += case item
+      when '1'
+        50
+      when '2'
+        65
+      when '3'
+        3
+      end
+    end
+  end
+
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -2,4 +2,14 @@ require 'checkout'
 
 describe Checkout do
 
+  describe '#checkout' do
+
+    context 'add to cost' do
+      it 'adds 24.95 for a water bottle' do
+        expect(subject.checkout(0001)).to eq 50
+      end
+    end
+
+  end
+
 end
