a23718ce0426b11242a0d59a1448bc8cb3a09eb9
Joshua Retallick <josh@old-empire.co.uk>
2021-03-16 16:51:11 +0000

    T14

--- spec/shop_spec.rb
+++ spec/shop_spec.rb
@@ -7,4 +7,12 @@ describe Shop do
     expect(shop).to be_a_kind_of Shop
   end
 
+  it "initializes a shop with transaction from the Transaction Class" do
+    expect(shop.new_transaction).to be_a_kind_of Transaction
+  end
+
+  it "initializes a shop with inventory from the Inventory Class" do
+    expect(shop.inventory).to be_a_kind_of Inventory
+  end
+
 end
